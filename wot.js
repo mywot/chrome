b270b97407f7e526a7f1e71d4a6b652f76bc2a6d
Sergey Andryukhin <sergey@mywot.com>
2012-11-06 09:45:34 +0200

    version 20121106

--- wot.js
+++ wot.js
@@ -19,7 +19,7 @@
 */
 
 var wot = {
-	version: 20121105,
+	version: 20121106,
 	platform: "chrome",
 	debug: false,   // when changing this, don't forget to switch ga_id value also!
 	default_component: 0,
