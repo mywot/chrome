34aeb257698f5355d35bceb6e3b609f7bffcc3dd
Sergey Andryukhin <sergey@mywot.com>
2013-01-21 12:52:28 +0200

    Updated version to 1.4.7 / 20130121

--- wot.js
+++ wot.js
@@ -19,7 +19,7 @@
 */
 
 var wot = {
-	version: 20130110,
+	version: 20130121,
 	platform: "chrome",
 	debug: false,           // when changing this, don't forget to switch ga_id value also!
 	default_component: 0,
