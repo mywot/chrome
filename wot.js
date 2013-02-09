751a35bad371976f99a60865a51a47e807923386
Sergey Andryukhin <sergey@mywot.com>
2013-02-09 16:58:59 +0200

    Cleaned orphaned files and updated version number

--- wot.js
+++ wot.js
@@ -19,7 +19,7 @@
 */
 
 var wot = {
-	version: 20130208,
+	version: 20130211,
 	platform: "chrome",
 	debug: false,           // when changing this, don't forget to switch ga_id value also!
 	default_component: 0,
