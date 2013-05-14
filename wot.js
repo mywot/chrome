989c2bb454095e143f2ad968407d037301e112ea
Sergey Andryukhin <sergey@mywot.com>
2013-05-14 11:30:02 +0300

    Updated to 2.0.1 / 20130514, small changes of texts, and update url is changed.

--- wot.js
+++ wot.js
@@ -19,7 +19,7 @@
 */
 
 var wot = {
-	version: 20130513,
+	version: 20130514,
 	platform: "chrome",
     locale: "en",           // cached value of the locale
     lang: "en-US",          // cached value of the lang
