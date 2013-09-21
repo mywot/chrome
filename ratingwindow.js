fce38401a83b6b35bb7422fd57f8346c75cfe339
Sergey Andryukhin <sergey@mywot.com>
2013-09-21 15:27:06 +0300

    removed redundant parentheses.

--- ratingwindow.js
+++ ratingwindow.js
@@ -266,7 +266,7 @@ $.extend(wot, { ratingwindow: {
             if (unload) {  // RW was closed by browser (not by clicking "Save")
 //                bg.console.log("RW triggered finish state during Unload");
 
-                if ((comment_changed)) {
+                if (comment_changed) {
 //                    bg.console.log("The comment seems to be changed");
                     // when comment body is changed, we might want to store it locally
                     bgwot.keeper.save_comment(target, user_comment, user_comment_id, votes, wot.keeper.STATUSES.LOCAL);
