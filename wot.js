2a513ea290c18505d3ed1e46d4551b41e8ce2c9b
Sergey Andryukhin <sergey@mywot.com>
2013-10-29 16:08:04 +0200

    Updated version 20131029 / 2.2.1

--- wot.js
+++ wot.js
@@ -19,7 +19,7 @@
 */
 
 var wot = {
-	version: 20131023,
+	version: 20131029,
 	platform: "chrome",
     locale: "en",           // cached value of the locale
     lang: "en-US",          // cached value of the lang
